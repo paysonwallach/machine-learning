14a0ab48b086cb3f6cbb3418f50b816b12d3102e
Payson Wallach <paysonwallach@icloud.com>
2017-07-25 00:13:13 -0700

    Removed extraneous code for validation and test data

--- matrix_network/mnist_loader.py
+++ matrix_network/mnist_loader.py
@@ -8,27 +8,15 @@ def load_data():
     training_data, validation_data, test_data = cPickle.load(data)
     data.close()
 
-    return (training_data, validation_data, test_data)
-
-
-def process_data():
-    training_data, validation_data, test_data = load_data()
-
     training_inputs = training_data[0]
     training_labels = training_data[1]
-    validation_inputs = validation_data[0]
-    validation_labels = validation_data[1]
-    test_inputs = test_data[0]
-    test_labels = test_data[1]
 
     for i in np.nditer(training_labels):
         training_labels[i] = vectorize_label(training_labels[i])
 
     training_data = zip(training_inputs, training_labels)
-    validation_data = zip(validation_inputs, validation_labels)
-    test_data = zip(test_inputs, test_labels)
 
-    return (training_data, validation_data, test_data)
+    return training_data, validation_data, test_data
 
 
 def vectorize_label(i):
