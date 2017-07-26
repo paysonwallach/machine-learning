e2f4d0a7c5e62ae829db5310a1526c9947718b16
Payson Wallach <paysonwallach@icloud.com>
2017-07-25 21:50:06 -0700

    Consolidated create_minibatch functions into mnist_loader

--- matrix_network/mnist_loader.py
+++ matrix_network/mnist_loader.py
@@ -19,6 +19,28 @@ def load_data():
     return training_data, validation_data, test_data
 
 
+def create_minibatch(data, minibatch_size):
+    inputs, labels = shuffle(data)
+
+    n = np.size(inputs[0], axis=0)
+
+    minibatch_inputs = np.empty(n, minibatch_size)
+    minibatch_labels = np.empty_like(labels)
+
+    for i in range(minibatch_size):
+        minibatch_inputs[:, i] = inputs[i]
+        minibatch_labels[i] = labels[i]
+
+    return minibatch_inputs, minibatch_labels
+
+
+def shuffle(data):
+    rng_state = np.random.get_state()
+    np.random.shuffle(data[0])
+    np.random.set_state(rng_state)
+    np.random.shuffle(data[1])
+
+
 def vectorize_label(i):
     label = np.zeros((10, 1))
     label[i] = 1.0
