1751526714bde9128d8acfd8dc316eb5150f818d
Payson Wallach <paysonwallach@icloud.com>
2017-08-03 21:34:49 -0700

    Updated to backpropagate bias error

--- matrix_network/matrix_network.py
+++ matrix_network/matrix_network.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
+np.set_printoptions(threshold=np.nan)
+
+
 def f_sigmoid(X, deriv=False):
     if not deriv:
         return 1 / (1 + np.exp(-X))
@@ -42,11 +45,11 @@ class Layer:
         if not input_layer:
             self.s = np.zeros((minibatch_size, size[1]))
             self.delta = np.zeros((minibatch_size, size[1]))
-            self.weights = np.random.randn(size[0], size[1])
+            self.weights = np.random.normal(size=size, scale=1e-4)
 
         if not input_layer and not output_layer:
             self.f_prime = np.zeros((minibatch_size, size[1]))
-            self.biases = np.random.randn(1, size[1])
+            self.biases = np.random.normal(size=[1, size[1]], scale=1)
 
     def forward_propagate(self):
         if self.input_layer:  # No activation function applied to input layer
@@ -54,14 +57,15 @@ class Layer:
 
         if not self.input_layer and not self.output_layer:
             self.s = np.dot(self.s, self.weights)
-            self.s += self.biases
+            self.s = np.add(self.s, self.biases)
             self.f_prime = self.activation(self.s, deriv=True)
 
         else:
             self.s = np.dot(self.s, self.weights)
 
         self.z = self.activation(self.s)
-
+        # print "S matrix: {0}\n{1}".format(np.shape(self.s), self.s[50, :])
+        # print "Z matrix: {0}\n{1}".format(np.shape(self.z), self.z[10, :])
         return self.z
 
 
@@ -92,8 +96,8 @@ class Network:
         print "Done!"
 
     def forward_propagate(self, input_data):
+        self.layers[0].z = input_data
         for i in range(self.num_layers-1):
-            print "Forward propagating through Layer {0}...".format(i)
             self.layers[i+1].s = self.layers[i].forward_propagate()
         return self.layers[-1].forward_propagate()
 
@@ -102,76 +106,91 @@ class Network:
         self.layers[-1].delta = (y_hat - classification)
 
         for i in range(self.num_layers-2, 0, -1):
-            print "Backpropagating through layer {0}...".format(i)
-
             self.layers[i].delta = np.dot(self.layers[i+1].delta,
                                           self.layers[i+1].weights.T) * \
                 self.layers[i].f_prime
 
     def update_weights(self, eta):
         for i in range(1, self.num_layers):
-            print "Updating layer {0}...".format(i)
-            """ del_b = -eta * np.dot(np.ones_like(self.layers[i].biases),
-                                  self.layers[i].delta) """
+            if i < self.num_layers-1:
+                self.layers[i].biases = np.add(self.layers[i].biases,
+                                               self.layers[i].delta)
 
-            del_w = -eta * np.dot(self.layers[i-1].z.T, self.layers[i].delta)
+                self.layers[i].del_b = -eta * np.dot(np.ones((1, 100)),
+                                                     self.layers[i].delta)
 
-            # self.layers[i].biases += del_b
-            self.layers[i].weights += del_w
+            self.layers[i].del_w = -eta * np.dot(self.layers[i-1].z.T,
+                                                 self.layers[i].delta)
 
-    def evaluate(self, training_data, test_data, epochs, eta,
-                 eval_training=False, eval_test=True):
-        training_examples = training_data[0]
-        n_training = len(training_examples)
-        n_test = len(test_data[1])
+            self.layers[i].weights = np.add(self.layers[i].weights,
+                                            self.layers[i].del_w)
+
+    def evaluate(self, training_data, validation_data, test_data,
+                 minibatch_size, epochs, eta, eval_training=False,
+                 eval_test=True):
+        n_training = len(training_data)
+        n_validation = len(validation_data)
+        n_test = len(test_data)
 
         print "Training for {0} epochs...".format(epochs)
         for t in range(epochs):
-            out_str = "Epoch {0}:".format(t+1)
+            np.random.shuffle(training_data)
 
-            for i in range(n_training/epochs):
+            out_str = "Epoch {0:2d}:".format(t+1)
+
+            for i in range(n_training/minibatch_size):
                 inputs, labels = create_minibatch(training_data, i,
-                                                  minibatch_size=100)
+                                                  minibatch_size,
+                                                  training_data=True)
                 output = self.forward_propagate(inputs)
                 self.backpropagate(output, labels)
                 self.update_weights(eta=eta)
 
             if eval_training:
                 errors = 0
-                training_inputs, training_labels = training_data
-                output = self.forward_propagate(training_inputs)
-                y_hat = np.argmax(output, axis=1)
-                errors += np.sum(1 - training_labels[np.arange(
-                                 len(training_labels)), y_hat])
+                for i in range(n_validation/minibatch_size):
+                    inputs, labels = create_minibatch(validation_data, i,
+                                                      minibatch_size)
+                    output = self.forward_propagate(inputs)
+                    y_hat = np.argmax(output, axis=1)
+                    errors += np.sum(y_hat == labels)
 
                 out_str = "{0} Training error: {1:.5f}".format(
-                    out_str, float(errors) / n_training)
+                    out_str, float(errors) / n_training * 100)
 
             if eval_test:
                 errors = 0
-                test_inputs, test_labels = test_data
-                output = self.forward_propagate(test_inputs)
+                for i in range(n_test/minibatch_size):
+                    inputs, labels = create_minibatch(test_data, i,
+                                                      minibatch_size)
+                output = self.forward_propagate(inputs)
                 y_hat = np.argmax(output, axis=1)
-                errors += np.sum(1 - test_labels[np.arange(
-                                 len(test_labels)), y_hat])
+                errors += np.sum(y_hat == labels)
 
                 out_str = "{0} Test error: {1:.5f}".format(
-                    out_str, float(errors) / n_test)
+                    out_str, float(errors) / n_test * 100)
 
             print out_str
 
 
-def create_minibatch(data, i, minibatch_size):
-    inputs = data[0]
-    labels = data[1]
+def create_minibatch(data, i, minibatch_size, training_data=False):
+    inputs, labels = zip(*data)
 
     n = np.size(inputs[0], axis=0)
 
-    minibatch_inputs = np.zeros((n, minibatch_size))
-    minibatch_labels = np.empty((minibatch_size, 10))
+    minibatch_inputs = np.zeros((minibatch_size, n))
+    if training_data:
+        minibatch_labels = np.empty((minibatch_size, 10))
 
-    for j in range(minibatch_size):
-        minibatch_inputs[:, j] = inputs[:, i+j]
-        minibatch_labels[j, :] = labels[i+j, :]
+        for j in range(minibatch_size):
+            minibatch_inputs[j, :] = inputs[i+j]
+            minibatch_labels[j, :] = labels[i+j]
+
+    else:
+        minibatch_labels = np.empty(minibatch_size)
+
+        for j in range(minibatch_size):
+            minibatch_inputs[j, :] = inputs[i+j]
+            minibatch_labels[j] = int(labels[i+j])
 
     return minibatch_inputs, minibatch_labels
