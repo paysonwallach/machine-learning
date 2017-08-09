Fixed error calculation

matrix_network/matrix_network.py
@@ -3,9 +3,25 @@ import numpy as np
 
 def f_sigmoid(X, deriv=False):
     if not deriv:
-        return 1 / (1 + np.exp(-X))
+        return 1.0 / (1.0 + np.exp(-X))
     else:
-        return f_sigmoid(X)*(1 - f_sigmoid(X))
+        return f_sigmoid(X)*(1.0 - f_sigmoid(X))
+
+
+def f_prelu(X, alpha=0.1, deriv=False):
+    if not deriv:
+        for x in np.nditer(X, op_flags=['readwrite']):
+            if x[...] <= 0:
+                x[...] *= alpha
+
+    else:
+        for x in np.nditer(X, op_flags=['readwrite']):
+            if x <= 0:
+                x[...] = alpha
+            else:
+                x[...] = 1
+
+    return X
 
 
 def f_softmax(X):
@@ -26,27 +42,29 @@ class Layer:
         self.activation = activation
         # Input matrix for the layer
         self.s = None
-        # Outgoing weight matrix (n x m)
+        # Incoming weight matrix (a x b)
         self.weights = None
         # Bias vector of the layer (n x 1)
         self.biases = None
         # Output matrix for the layer
         self.z = np.zeros((minibatch_size, size[0]))
-        # Gradient of outgoing weight matrix (n x m)
+        # Gradient of incoming weight matrix (a x b)
         self.del_w = None
         # Gradient of the bias vector of the layer (n x 1)
         self.del_b = None
-        # Activation function derivatives for the layer (n x m)
+        # Activation function derivatives for the layer (n x b)
         self.f_prime = None
 
         if not input_layer:
             self.s = np.zeros((minibatch_size, size[1]))
             self.delta = np.zeros((minibatch_size, size[1]))
-            self.weights = np.random.normal(size=size, scale=1e-4)
+            self.weights = np.random.normal(size=size, scale=1E-4)
+            self.del_w = np.empty_like(self.weights)
 
         if not input_layer and not output_layer:
             self.f_prime = np.zeros((minibatch_size, size[1]))
-            self.biases = np.random.normal(size=[1, size[1]], scale=1)
+            self.biases = np.random.normal(size=[1, size[1]], scale=1E-4)
+            self.del_b = np.empty_like(self.biases)
 
     def forward_propagate(self):
         if self.input_layer:  # No activation function applied to input layer
@@ -61,8 +79,7 @@ class Layer:
             self.s = np.dot(self.s, self.weights)
 
         self.z = self.activation(self.s)
-        # print "S matrix: {0}\n{1}".format(np.shape(self.s), self.s[50, :])
-        # print "Z matrix: {0}\n{1}".format(np.shape(self.z), self.z[10, :])
+
         return self.z
 
 
@@ -76,17 +93,17 @@ class Network:
         print "Initilizing network..."
         for i in range(self.num_layers-1):
             if i == 0:
-                print "Initilizing input layer of size {0}.".format(
+                print "\tInitilizing input layer of size {0}.".format(
                     sizes[i])
                 self.layers[i] = Layer([sizes[i]], minibatch_size,
                                        input_layer=True)
             else:
-                print "Initilizing hidden layer of size {0}.".format(
+                print "\tInitilizing hidden layer of size {0}.".format(
                     sizes[i])
                 self.layers[i] = Layer([sizes[i-1], sizes[i]], minibatch_size,
                                        activation=f_sigmoid)
 
-        print "Initilizing output layer of size {0}.".format(sizes[-1])
+        print "\tInitilizing output layer of size {0}.".format(sizes[-1])
         self.layers[-1] = Layer([sizes[-2], sizes[-1]], minibatch_size,
                                 output_layer=True, activation=f_softmax)
 
@@ -133,7 +150,7 @@ class Network:
         for t in range(epochs):
             np.random.shuffle(training_data)
 
-            out_str = "Epoch {0:2d}:".format(t+1)
+            out_str = "\tEpoch {0:2d}:".format(t+1)
 
             for i in range(n_training/minibatch_size):
                 inputs, labels = create_minibatch(training_data, i,
@@ -144,28 +161,28 @@ class Network:
                 self.update_weights(eta=eta)
 
             if eval_training:
-                errors = 0
+                n_correct = 0
                 for i in range(n_validation/minibatch_size):
                     inputs, labels = create_minibatch(validation_data, i,
                                                       minibatch_size)
                     output = self.forward_propagate(inputs)
                     y_hat = np.argmax(output, axis=1)
-                    errors += np.sum(y_hat == labels)
+                    n_correct += np.sum(y_hat == labels)
 
-                out_str = "{0} Training error: {1:.5f}".format(
-                    out_str, float(errors) / n_training * 100)
+                out_str = "{0} Training accuracy: {1:.2f}%".format(
+                    out_str, float(n_correct)/n_validation * 100)
 
             if eval_test:
-                errors = 0
+                n_correct = 0
                 for i in range(n_test/minibatch_size):
                     inputs, labels = create_minibatch(test_data, i,
                                                       minibatch_size)
-                output = self.forward_propagate(inputs)
-                y_hat = np.argmax(output, axis=1)
-                errors += np.sum(y_hat == labels)
+                    output = self.forward_propagate(inputs)
+                    y_hat = np.argmax(output, axis=1)
+                    n_correct += np.sum(y_hat == labels)
 
-                out_str = "{0} Test error: {1:.5f}".format(
-                    out_str, float(errors) / n_test * 100)
+                out_str = "{0} Test accuracy: {1:.2f}%".format(
+                    out_str, float(n_correct)/n_test * 100)
 
             print out_str
 